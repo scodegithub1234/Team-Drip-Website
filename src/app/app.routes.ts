9d636c3be16dcefda56c31d813bc55fb35b71b0e
Bonnie <bonhuynh@umich.edu>
2024-11-24 13:15:10 -0500

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DailyHealthComponent } from './daily-health/daily-health.component';
+import { DietTrackerComponent } from './diet-tracker/diet-tracker.component';
+import { PamphletComponent } from './pamphlet/pamphlet.component';
+import { MentalHealthComponent } from './mental-health/mental-health.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pamphlet', component: PamphletComponent },
+  { path: 'daily-health', component: DailyHealthComponent },
+  { path: 'diet-tracker', component: DietTrackerComponent },
+  { path: 'mental-health', component: MentalHealthComponent }
+];
